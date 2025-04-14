35bf2e007d9245d7a87d9cb61defd42885002a43
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-14 08:24:20 -0300

    Arquivo 'main.py' criado e configurado com sucesso

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+
+def gerar_numero_de_clientes():
+    mensagens = [
+        "Número baixo! - Dois caixas abertos.",
+        "Número médio! - Tres caixas abertos",
+        "Número alto! - Quatro caixas abertos",
+        "Número muito alto! - Cinco caixas abertos",
+        "Número máximo! - Seis caixas abertos",
+    ]
+
+    ultimo_numero = -1
+    while True:
+        clientes = random.randint(10, 60)
+        print(f"Número de clientes no mercado: {clientes}")
+
+        if ultimo_numero == -1 or abs(clientes - ultimo_numero) >= 20:
+            if clientes < 20:
+                print(mensagens[0])
+            elif clientes < 30:
+                print(mensagens[1])
+            elif clientes < 40:
+                print(mensagens[2])
+            elif clientes < 50:
+                print(mensagens[3])
+            else:
+                print(mensagens[4])
+
+            ultimo_numero = clientes
+
+        time.sleep(5)
+
+
+gerar_numero_de_clientes()
